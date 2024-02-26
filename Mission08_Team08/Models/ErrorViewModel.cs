db423966436b7330f82cb43c8d2e94efac832549
jacobmillett <jakemillett1@gmail.com>
2024-02-26 10:08:18 -0700

    Add project files.

---
+++ Mission08_Team08/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission08_Team08.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
