893731533156a441003699c9def6cfcbd611bffc
stewyman <stewyman@REEDSTEWARTE3F6>
2024-02-29 17:51:53 -0700

    services setup

--- Mission08_Team08/Program.cs
+++ Mission08_Team08/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Mission08_Team08.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<TaskApplicationContext>(options =>
+{
+    options.UseSqlite(builder.Configuration["ConnectionStrings:Connect"]);
+});
 
 var app = builder.Build();
 
