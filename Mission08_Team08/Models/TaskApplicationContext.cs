6b6c28b3c193ef8e2ef9d241e39245c0858b74dc
stewyman <stewyman@REEDSTEWARTE3F6>
2024-02-29 17:40:47 -0700

    context file

--- Mission08_Team08/Models/TaskApplicationContext.cs
+++ Mission08_Team08/Models/TaskApplicationContext.cs
@@ -4,5 +4,10 @@ namespace Mission08_Team08.Models
 {
     public class TaskApplicationContext : DbContext
     {
+        public TaskApplicationContext(DbContextOptions<TaskApplicationContext> options) : base (options)
+        {
+        }
+
+        public DbSet<Task> Tasks { get; set; }
     }
 }
